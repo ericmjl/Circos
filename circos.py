ef6458e6445af050a0c4a4f13b8b9428af18f71b
Eric Ma <ericmajinglong@gmail.com>
2015-02-25 20:27:20 -0500

    Added new features
    1. Specify a figure or ax to draw into.
    2. Specify node colors
    3. Specify line width of the nodes to be 0.
    4. Changed paths from CURVE4 to CURVE3.

--- circos.py
+++ circos.py
@@ -6,12 +6,19 @@ import math
 from matplotlib.path import Path
 
 class CircosPlot(object):
-	def __init__(self, nodes, edges, radius):
+	def __init__(self, nodes, edges, radius, nodecolor='black', figsize=(8,8), ax=None, fig=None):
 		self.nodes = nodes # Dictionary of nodes
 		self.edges = edges # Dictionary of Edges
+		self.nodecolor = nodecolor
 		self.radius = radius
-		self.fig = plt.figure(figsize=(8,8))
-		self.ax = self.fig.add_subplot(111)
+		if fig == None:
+			self.fig = plt.figure(figsize=figsize)
+		else:
+			self.fig = fig
+		if ax == None:
+			self.ax = self.fig.add_subplot(111)
+		else:
+			self.ax = ax
 		self.node_radius = self.radius*0.05
 		self.ax.set_xlim(-radius*1.05, radius*1.05)
 		self.ax.set_ylim(-radius*1.05, radius*1.05)
@@ -31,17 +38,20 @@ class CircosPlot(object):
 		for node in self.nodes:
 			theta = self.node_theta(node)
 			x, y = get_cartesian(r, theta)
-			node_patch = patches.Ellipse((x,y), node_r, node_r, facecolor='red')
+			node_patch = patches.Ellipse((x,y), node_r, node_r, facecolor=self.nodecolor, lw=0)
 			self.ax.add_patch(node_patch)
 
 
 	def draw_edge(self, node1, node2):
 		start_theta = self.node_theta(node1)
 		end_theta = self.node_theta(node2)
-		middle_theta = float((start_theta + end_theta))/2.0
+		middle_theta = (start_theta + end_theta)/2.0
+		delta_theta = abs(end_theta - start_theta)
+		middle_r = self.radius * (1 - delta_theta / np.pi) 
 
+		# verts = [get_cartesian(self.radius, start_theta), get_cartesian(middle_theta, middle_r), get_cartesian(self.radius,end_theta)]
 		verts = [get_cartesian(self.radius, start_theta), (0,0), get_cartesian(self.radius,end_theta)]
-		codes = [Path.MOVETO, Path.CURVE4, Path.CURVE4]
+		codes = [Path.MOVETO, Path.CURVE3, Path.CURVE3]
 
 		path = Path(verts, codes)
 
